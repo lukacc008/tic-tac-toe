e1209d4333f3a06bceaecd8e3d78e5781fc41634
unknown <lukacc008@gmail.com>
2024-02-06 15:57:31 +0100

    Using state and conditional rendering

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,15 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  // let btnCaption = 'Edit';
 
   if(isEditing) {
-    playerName = <input type="text" required />
+    playerName = <input type="text" required value={name} />
+    // btnCaption = 'Save';
   }
 
   return (
@@ -19,63 +21,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
-
-// export default function Player({ name, symbol }) {
-//     const [buttonState, setButtonState] = useState(true);
-//     const [nameState, setNameState] = useState('');
-
-//     function btnState() {
-//       setButtonState(!buttonState);
-//     }
-
-//     function updateName() {
-//       setNameState()
-//     }
-
-//     return (
-//       <li>
-//         <span className="player">
-//           {buttonState ? (<span className="player-name">{name}</span>) : (<input onSubmit={updateName}></input>)}
-//           <span className="player-symbol">{symbol}</span>
-//         </span>
-//         {buttonState ? (
-//           <button onClick={btnState}>Edit</button>
-//         ) : (
-//           <button onClick={updateName}>Save</button>
-//         )}
-//       </li>
-//     );
-//   }
-
-// import { useState } from "react";
-
-// export default function Player({ name, symbol }) {
-//   const [buttonState, setButtonState] = useState(true);
-//   const [nameState, setNameState] = useState('');
-
-//   function btnState() {
-//     setButtonState(!buttonState);
-//   }
-
-//   function updateName() {
-//     setNameState()
-//   }
-
-//   return (
-//     <li>
-//       <span className="player">
-//         {buttonState ? (<span className="player-name">{name}</span>) : (<input onSubmit={updateName}></input>)}
-//         <span className="player-symbol">{symbol}</span>
-//       </span>
-//       {buttonState ? (
-//         <button onClick={btnState}>Edit</button>
-//       ) : (
-//         <button onClick={updateName}>Save</button>
-//       )}
-//     </li>
-//   );
-// }
