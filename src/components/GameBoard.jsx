7e91056f11160ba85c9d82b010ea179fbffeb6f5
unknown <lukacc008@gmail.com>
2024-02-16 15:16:19 +0100

    Combinations work, and we are getting a winner now. Need to figure out how to stop the game after we have a winner

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,20 +1,9 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
 
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
+export default function GameBoard({ onSelectSquare, board }) {
+  
 
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -30,7 +19,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
